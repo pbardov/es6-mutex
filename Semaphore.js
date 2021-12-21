8d86917d73c69f030a7c0f7b2321f95078be52dc
Pavel <pbardov@gmail.com>
2021-12-22 01:54:59 +0500

    Улучшен механизм захвата/освобождения Semaphore

--- Semaphore.js
+++ Semaphore.js
@@ -2,11 +2,36 @@
 const { EventEmitter } = require('events');
 
 class Semaphore extends EventEmitter {
+  static immediate() {
+    const promise = new Promise((resolve) => {
+      promise.immediateID = setImmediate(resolve);
+    });
+    return promise;
+  }
+
   constructor(max) {
     super();
 
     this.n = 0;
     this.max = max;
+  }
+
+  async waitComplete() {
+    const self = this.constructor;
+    while (this.n > 0) {
+      await self.immediate();
+    }
+  }
+
+  async done() {
+    return this.waitComplete();
+  }
+
+  async waitRelease() {
+    const self = this.constructor;
+    while (this.n >= this.max) {
+      await self.immediate();
+    }
   }
 
   async onRelease() {
@@ -22,15 +47,14 @@ class Semaphore extends EventEmitter {
   }
 
   async acquire() {
-    while (this.n + 1 > this.max) {
-      await this.onRelease();
-    }
+    await this.waitRelease();
     this.n += 1;
     this.emit('acquire', this.n);
   }
 
   release() {
     this.n -= 1;
+    if (this.n < 0) this.n = 0;
     this.emit('release', this.n);
   }
 
@@ -38,11 +62,9 @@ class Semaphore extends EventEmitter {
     await this.acquire();
     try {
       const result = await fn(...args);
-      this.release();
       return result;
-    } catch (err) {
+    } finally {
       this.release();
-      throw err;
     }
   }
 }
